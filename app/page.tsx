1f83ea609bbc6f7de0ceb6c4c6f360ac99d07cc1
Faizaan Qureshi <qfaizaan@gmail.com>
2024-11-26 01:52:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ const CARDS = [
       <div className="flex flex-col md:flex-row gap-4">
         <Image src="/ali.png" height={96} width={96} alt="Ali" className="rounded-full"></Image>
         <p className="text-black text-sm">
-          A <Highlight>fourth year</Highlight> student working in project management at Ontario Power Generation. He has a good grasp on <Highlight>physic</Highlight> and <Highlight>chemistry</Highlight>, from kinematics to organic.
+          A <Highlight>fourth year</Highlight> student working in project management at Ontario Power Generation. He has a good grasp on <Highlight>physics</Highlight> and <Highlight>chemistry</Highlight>, from kinematics to organic.
         </p>
       </div>
     ),
