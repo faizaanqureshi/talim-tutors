2021cef5bc408f5020dee95adaea244f358c1b6e
Faizaan Qureshi <qfaizaan@gmail.com>
2024-11-25 23:25:44 -0500

    add calendly

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -30,17 +30,17 @@ export default function Modal() {
             <DialogTitle></DialogTitle>
             <DialogContent className="w-11/12 md:w-3/5 py-12">
                 <div className="flex flex-col space-between w-full items-center">
-                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-center">
+                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-center text-stone-700">
                         Not sure if you need tutoring?
                     </h3>
-                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-center">
+                    <h3 className="scroll-m-20 text-xl md:text-2xl font-semibold tracking-tight text-center text-stone-700">
                         Still unsure about pricing or quality?
                     </h3>
-                    <p className="leading-7 [&:not(:first-child)]:mt-6 text-center text-lg">
+                    <p className="leading-7 [&:not(:first-child)]:mt-6 text-center text-lg text-stone-700">
                         Experience a free lesson and see why our tutoring stands out. No obligations, just quality education.
                     </p>
-                    <a href="mailto:talimtutoring@gmail.com">
-                        <Button type="submit" onClick={handleClose} className="mt-12 bg-blue-700 text-md md:text-lg font-bold ">
+                    <a href="https://calendly.com/talimtutoring/30min">
+                        <Button type="submit" onClick={handleClose} className="mt-12 bg-blue-600 text-md md:text-lg font-bold ">
                             Schedule Your Free Consultation
                         </Button>
                     </a>
